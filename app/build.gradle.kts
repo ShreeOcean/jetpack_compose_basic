5b956349e755fda8a9e766c0b48709fe95cfaf6e
PRAGYANSHREE DAS <pragyan9423@gmail.com>
2024-10-08 12:04:41 +0530

    jetpack compose state hosting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation( libs.material3)
+    implementation (libs.androidx.material.icons.core)
+    implementation (libs.androidx.material.icons.extended)
 }
--- app/src/main/java/com/ocean/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/ocean/jetpackcomposebasic/MainActivity.kt
@@ -1,31 +1,40 @@
 package com.ocean.jetpackcomposebasic
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ocean.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
@@ -36,9 +45,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeBasicTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyAPP(modifier = Modifier.fillMaxSize())
-                }
+                MyAPP(modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -46,41 +53,50 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
 
-    val expand = remember {
+@Composable
+private fun CardContent(name: String){
+    var expanded by rememberSaveable {
         mutableStateOf(false)
     }
-        val extraPadding = if (expand.value) 45.dp else 0.dp
 
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.padding(vertical = 2.dp, horizontal = 4.dp)
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
     ) {
-        Row(modifier = modifier
-            .fillMaxWidth()
-            .padding(14.dp)) {
-            Column(
-                modifier = modifier.weight(1f).padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello")
-                Text(text = "$name")
+        Column(modifier = Modifier
+            .weight(1f)
+            .padding(12.dp)) {
+            Text(text = "hello, ")
+            Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                fontWeight = FontWeight.ExtraBold
+            ))
+            
+            if (expanded){
+                Text(text = ("Compose uses colors in a lazy way." + "Padding is themed nicely, with bouncy animations.").repeat(4))
             }
-            ElevatedButton(
-                onClick = { expand.value = !expand.value },//){
-                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.onSecondary)) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Icon(painter = painterResource(id = if (expand.value) R.drawable.icon_expand_less else R.drawable.icon_expand_more),
-                        contentDescription = "show more icon",
-                        modifier = modifier.size(20.dp),
-                        tint = MaterialTheme.colorScheme.primary
-                        )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(
-                        text = if (expand.value)"Show Less" else "Show More",
-                        color = MaterialTheme.colorScheme.primary
-                        )
-                }
-            }
+        }
+
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) stringResource(id = R.string.show_less) else stringResource(id = R.string.show_more)
+            )
         }
     }
 }
@@ -88,19 +104,74 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun MyAPP(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("name","NA","compose")
 ) {
-    Column(modifier.padding(vertical = 4.dp)) {
-        for (name in names){
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+
+}
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Unit) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to Basics Code lab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick =  onContinueClicked,
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000){"$it"}
+){
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names){ name ->
             Greeting(name = name)
         }
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    JetpackComposeBasicTheme {
+        OnboardingScreen(onContinueClicked = {}) //Do nothing click.
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark"
+)
+@Preview( showBackground = true, widthDp = 320, heightDp = 320 )
 @Composable
 fun GreetingPreview() {
     JetpackComposeBasicTheme {
-        MyAPP()
+        Greetings()
+    }
+}
+@Preview
+@Composable
+fun MyAppPreview(){
+    JetpackComposeBasicTheme {
+        MyAPP(Modifier.fillMaxSize())
     }
 }
